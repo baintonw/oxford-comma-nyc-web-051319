38cffbbdbf75f2c523a65c3cc5d88cb90a02a0d6
William Bainton <baintonw@gmail.com>
2019-05-03 23:24:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif
+    array.length == 2
+    array.join(" and ")
+  else
   first = array[0...-1].join(", ")
   first
   last = array[-1].to_s
